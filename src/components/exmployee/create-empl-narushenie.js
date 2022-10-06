f730061751acafab7a80f56f31a9408684543a30
User <aymirzajumatashov@gmail.com>
2022-10-06 11:51:39 +0500

    save save all data to server (localhost:4041/api/upload) create-empl-narushenie.js

--- src/components/exmployee/create-empl-narushenie.js
+++ src/components/exmployee/create-empl-narushenie.js
@@ -1,45 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
-const Form = () => {
-  // a local state to store the currently selected file.
-  const [selectedFile, setSelectedFile] = React.useState(null);
-  const [lastname, setLastname] = React.useState(null);
-  const [firstname, setFirstname] = React.useState(null);
+const CreateEmplNarushenie = () => {
+    const [result, setResult] = useState("");
+    const handleImageUpload = (event) => {
+        event.preventDefault();
+        const file = event.currentTarget["fileInput"].files[0];
 
-  const handleSubmit = async(event) => {
-    event.preventDefault()
-    const formData = new FormData();
-    formData.append("lastname",lastname);
-    formData.append("firstname",firstname);
-    // formData.append("uchastka",uchastka);
-    // formData.append("tsex_uchastka",tsex_uchastka);
-    // formData.append("pravila",pravila);
-    // formData.append("narushenie",narushenie);
-    formData.append("selectedFile", selectedFile);
-    try {
-      const response = await axios({
-        method: "post",
-        url: "http://localhost:4041/api/upload",
-        data: formData,
-        headers: { "Content-Type": "multipart/form-data" },
-      });
-    } catch(error) {
-      console.log(error)
-    }
-  }
+        const formData = new FormData();
+        formData.append("lastname", "lastname");
+        formData.append("firstname", "firstname");
+        formData.append("uchastka", "uchastka");
+        formData.append("tsex_uchastka", "tsex_uchastka");
+        formData.append("pravila", "pravila");
+        formData.append("narushenie", "narushenie");
+        formData.append("file", file);
 
-  const handleFileSelect = (event) => {
-    setSelectedFile(event.target.files[0])
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name={lastname}/>
-      <input type="file" onChange={handleFileSelect}/>
-      <input type="submit" value="Upload File" />
-    </form>
-  )
+        fetch("http://localhost:4041/api/upload", {
+            method: "POST",
+            body: formData
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setResult(data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+    return (
+        <div>
+            <form onSubmit={handleImageUpload}>
+                <input id="fileInput" type="file"/>
+                <input type="submit"/>
+            </form>
+            <br/>
+            <br/>
+            Result:
+            <br/>
+            <pre>{JSON.stringify(result, null, 2)}</pre>
+        </div>
+    );
 };
+export default CreateEmplNarushenie;
 
-export default Form;
+
